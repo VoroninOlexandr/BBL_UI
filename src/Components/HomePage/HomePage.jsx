ec41402d79bd6415e43fe6e5e7e584e35d8bf5c1
DashavskiyVolodymyr <146772901+DashavskiyVolodymyr@users.noreply.github.com>
2025-03-31 16:16:41 +0300

    Revert "Revert 20 feature/poker 32"

---
+++ src/Components/HomePage/HomePage.jsx
@@ -0,0 +1,134 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import "./HomePage.css";
+
+const HomePage = () => {
+  const [lobbies, setLobbies] = useState([]);
+  const [newLobbyName, setNewLobbyName] = useState("");
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+
+ useEffect(() => {
+    // Блокування масштабування при натисканні Ctrl + Scroll
+    const handleWheel = (e) => {
+      if (e.ctrlKey) {
+        e.preventDefault(); // Запобігаємо стандартному дії масштабування
+      }
+    };
+
+    // Додаємо обробник події
+    window.addEventListener("wheel", handleWheel, { passive: false });
+
+    // Очищуємо обробник події при розмонтуванні компонента
+    return () => {
+      window.removeEventListener("wheel", handleWheel);
+    };
+  }, []); // Виконати тільки один раз після рендеру компонента
+  
+  useEffect(() => {
+    fetchLobbies();
+  }, []);
+
+
+
+  const fetchLobbies = async () => {
+    try {
+      const response = await axios.get("http://localhost:8080/api/games/get-all");
+      setLobbies(response.data);
+    } catch (err) {
+      setError("Failed to fetch game lobbies");
+    }
+  };
+
+  const handleAddLobby = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post("http://localhost:8080/api/games/create", { lobbyName: newLobbyName });
+      setNewLobbyName("");
+      fetchLobbies();
+    } catch (err) {
+      setError("Failed to create a new lobby");
+    }
+  };
+
+  const handleJoinTable = async (lobbyId) => {
+    const username = sessionStorage.getItem("username");
+    if (!username) {
+      setError("You must be logged in to join a lobby.");
+      return;
+    }
+    try {
+      const response = await axios.post(`http://localhost:8080/api/games/join/${lobbyId}`, { playerName: username });
+      const { playerId, lobbyId: gameId } = response.data;
+      if (!playerId || !gameId) {
+        setError("Failed to join the game. Invalid server response.");
+        return;
+      }
+      sessionStorage.setItem("playerId", playerId);
+      sessionStorage.setItem("gameId", gameId);
+      sessionStorage.setItem("lobbyId", lobbyId);
+      
+      navigate(`/table/${playerId}`);
+    } catch (err) {
+      setError("Failed to join the table.");
+    }
+  };
+
+  return (
+    <div className="home-page">
+      <h4>Lobby List</h4>
+      <h3>Choose available lobby from the list or create one</h3>
+      {error && <div className="error-message">{error}</div>}
+      <form onSubmit={handleAddLobby} className="create-lobby-form">
+        <input
+          type="text"
+          value={newLobbyName}
+          onChange={(e) => setNewLobbyName(e.target.value)}
+          placeholder="Enter lobby name"
+          required
+          className="lobby-input"
+        />
+        <button type="submit" className="create-lobby-button">Create</button>
+      </form>
+      <table className="lobby-table">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Number of Players</th>
+            <th>State</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {lobbies.length === 0 ? (
+            <tr>
+              <td colSpan="4" className="no-lobbies">No lobbies found</td>
+            </tr>
+          ) : (
+            lobbies.map((lobby) => (
+              <tr key={lobby.id}>
+                <td>{lobby.lobbyName}</td>
+                <td>{lobby.playerCount}</td>
+                <td >
+                {lobby.playerCount >= 6 ? '🔒' : '🔓'}
+                </td>
+                <td>
+                <button
+                onClick={() => handleJoinTable(lobby.id)}
+                disabled={lobby.playerCount >= 6} 
+                className={`join-lobby-button ${lobby.playerCount >= 6 ? 'disabled' : ''}`}
+                >
+                Join
+                </button>
+                </td>
+              </tr>
+            ))
+          )}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default HomePage;
---
+++ src/Components/WebSocketComponent/WebsocketComponentDealer.jsx
@@ -0,0 +1,109 @@
+import React, { useEffect, useState } from "react";
+import "./WebSocketComponentDealer.css";
+import WebSocketService from "../../WebSocketService";
+
+const Suit = {
+  0: "clubs",
+  1: "diamonds",
+  2: "hearts",
+  3: "spades",
+};
+
+const Rank = {
+  0: "TWO",
+  1: "THREE",
+  2: "FOUR",
+  3: "FIVE",
+  4: "SIX",
+  5: "SEVEN",
+  6: "EIGHT",
+  7: "NINE",
+  8: "TEN",
+  9: "JACK",
+  10: "QUEEN",
+  11: "KING",
+  12: "ACE",
+};
+
+const WebSocketComponentDealer = ({ players }) => {
+  const [communityCards, setCommunityCards] = useState([]);
+  const [privateCards, setPrivateCards] = useState([]);
+  const [pot, setPot] = useState(0);
+  const [playerBalances, setPlayerBalances] = useState(
+    players.map((player) => ({ ...player, balance: 1000}))
+  );
+  const webSocketService = new WebSocketService();
+
+  const handleCardData = (data) => {
+    if (data.actionType === 1) {
+      const newCard = {
+        suit: data.ordinalCard.suit,
+        rank: data.ordinalCard.rank,
+      };
+
+      setCommunityCards((prev) => [...prev, newCard]);
+    }
+    
+    else if (data.actionType === 0) {
+      const newCard = {
+        suit: data.ordinalCard.suit,
+        rank: data.ordinalCard.rank,
+      };
+
+      setPrivateCards((prev) => [...prev, newCard]);
+    }
+    
+    else if (data.actionType === 3) {
+      console.log("Carti");
+      const { playerId, amount, newPot } = data;
+      setPlayerBalances((prevBalances) =>
+        prevBalances.map((player) =>
+          player.id === playerId ? { ...player, balance: player.balance - amount } : player
+        )
+      );
+
+      setPot(() => newPot);
+      }
+  };
+
+  useEffect(() => {
+    const gameId = sessionStorage.getItem("lobbyId");
+    const playerId = sessionStorage.getItem("playerId");
+
+    if (gameId && playerId) {
+      webSocketService.connect(gameId, playerId, handleCardData);
+    }
+  }, []);
+
+  return (
+    <div className="game-table-container">
+      <div className="pot-container">Pot: ${pot}</div>
+
+      <div className="cards-container">
+        {communityCards.map((card, index) => (
+          <div key={index} className="card">
+            <img
+              src={`/src/Components/Assets/suits/${Suit[card.suit]}_${Rank[card.rank]}.png`}
+              alt={`${Rank[card.rank]} of ${Suit[card.suit]}`}
+              className="card-image"
+            />
+          </div>
+        ))}
+      </div>
+
+      <div className="private-cards-container">
+        {privateCards.map((card, index) => (
+          <div key={index} className="card">
+            <img
+              src={`/src/Components/Assets/suits/${Suit[card.suit]}_${Rank[card.rank]}.png`}
+              alt={`${Rank[card.rank]} of ${Suit[card.suit]}`}
+              className="card-image"
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default WebSocketComponentDealer;
